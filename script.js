cc2aa6da91cc3696f53253da6c33abf16db253b3
chanwinharold <153543115+chanwinharold@users.noreply.github.com>
2025-01-21 13:30:38 +0100

    refactoring script.js code and correction for HTML semantics tag

--- script.js
+++ script.js
@@ -18,64 +18,38 @@ function generateCard() {
     allArticles.forEach((card) => {
 
         // Création des Tags HTML
-        let article = document.createElement("article")
-        let paragraph = document.createElement("p")
-        let div = document.createElement("div")
-        let iconLink = document.createElement("a")
-        let titleContainer = document.createElement("p")
-        let icon = document.createElement("img")
-        let mainTitle = document.createElement("h2")
-        let secondaryTitle = document.createElement("h3")
+        let mainTitle = document.createElement("h1")
+        let secondaryTitle = document.createElement("h2")
         let detailsParagraph = document.createElement("p")
+        let titleContainer = document.createElement("p")
+        let paragraph = document.createElement("p")
+        let buttonLink = document.createElement("button")
+        let article = document.createElement("article")
+        let div = document.createElement("div")
+        let icon = document.createElement("img")
 
         // Ajouter le contenu de chaque tag
         icon.src = "images/icon-ellipsis.svg";
+        icon.alt = "ellipsis";
         secondaryTitle.innerText = card.title
         mainTitle.innerText =  `${card.timeframes.weekly.current}hrs`
         detailsParagraph.innerText = `Last week - ${card.timeframes.weekly.previous}hrs`
         article.style.backgroundColor = card.color
         article.style.backgroundImage = "url(" + `${card.image}` + ")"
-        iconLink.href = "#"
 
         // Écouteurs d'événement pour définir la timeframe [daily, weekly, monthly]
-        btnDaily.addEventListener("click", () => {
-            mainTitle.innerText =  `${card.timeframes.daily.current}hrs`
-            detailsParagraph.innerText = `Last day - ${card.timeframes.daily.previous}hrs`
+        listenBtnTimeframe(btnDaily, card.timeframes.daily.current, card.timeframes.weekly.current, mainTitle, detailsParagraph)
+        listenBtnTimeframe(btnWeekly, card.timeframes.weekly.current, card.timeframes.weekly.previous, mainTitle, detailsParagraph)
+        listenBtnTimeframe(btnMonthly, card.timeframes.monthly.current, card.timeframes.monthly.previous, mainTitle, detailsParagraph)
 
-            // Changer la couleur du lien
-            btnDaily.classList.add("white")
-            btnWeekly.classList.remove("white")
-            btnMonthly.classList.remove("white")
-        })
-        btnWeekly.addEventListener("click", () => {
-            mainTitle.innerText =  `${card.timeframes.weekly.current}hrs`
-            detailsParagraph.innerText = `Last week - ${card.timeframes.weekly.previous}hrs`
-
-            // Changer la couleur du lien
-            btnDaily.classList.remove("white")
-            btnWeekly.classList.add("white")
-            btnMonthly.classList.remove("white")
-        })
-        btnMonthly.addEventListener("click", () => {
-            mainTitle.innerText =  `${card.timeframes.monthly.current}hrs`
-            detailsParagraph.innerText = `Last month - ${card.timeframes.monthly.previous}hrs`
-
-            // Changer la couleur du lien
-            btnDaily.classList.remove("white")
-            btnWeekly.classList.remove("white")
-            btnMonthly.classList.add("white")
-        })
-
-        // Attribuer à chaque parent l'enfant correspondant la première ligne "flex : row" de la carte
-        iconLink.appendChild(icon)
+        // Attribuer à chaque parent l'enfant correspondant (première ligne "flex : row" de la carte)
+        buttonLink.appendChild(icon)
         titleContainer.appendChild(secondaryTitle)
-        titleContainer.appendChild(iconLink)
+        titleContainer.appendChild(buttonLink)
 
         // Mettre tous les tags dans un tableau et les ajouter un à un dans la div de la carte
         let arrayElement = [titleContainer, mainTitle, detailsParagraph]
-        arrayElement.forEach((element) => {
-            div.appendChild(element)
-        })
+        arrayElement.forEach(element=> div.appendChild(element))
 
         // Ajouter tous les élements à l'article
         article.appendChild(paragraph)
@@ -83,5 +57,24 @@ function generateCard() {
 
         // Ajouter chaque article à la zone d'affichage des cartes
         zoneCard.appendChild(article);
+    })
+}
+
+function listenBtnTimeframe(button, currentTimeframe, previousTimeFrame, mainTitle, detailsParagraph) {
+    button.addEventListener("click", () => {
+        mainTitle.innerText =  `${currentTimeframe}hrs`
+        detailsParagraph.innerText = `Last day - ${previousTimeFrame}hrs`
+
+        btnColor(button, btnDaily, btnWeekly, btnMonthly )
+    })
+}
+
+function btnColor(button, button1, button2, button3 ) {
+    let arrayBtn = [button1, button2, button3]
+    arrayBtn.forEach((btn) => {
+        if (btn === button)
+            btn.classList.add("white")
+        else
+            btn.classList.remove("white")
     })
 }
